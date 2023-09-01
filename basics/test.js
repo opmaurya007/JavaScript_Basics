cd541dae329a45c346ac789f26be417fce38398a
OMPRAKASH KUSHWAHA <124498524+opmaurya007@users.noreply.github.com>
2023-09-01 18:03:16 +0000

    date and array

--- basics/test.js
+++ basics/test.js
@@ -5,3 +5,18 @@ var accountPassword = "12345"
 accountCity = "Pune"
 // accountId = 2
 console.log(accountId);
+//++++++++++++
+//stack(Primitive),Heap (Non-primitive)
+
+//date and time
+let myDate = new Date();
+// console.log(myDate);
+// console.log(myDate.toString()); 
+// console.log(myDate.getDay);
+//  console.log(myDate.setFullYear);
+// console.log(myDate.toLocaleString());
+//Array
+
+const myArray = [1,2,3,4];
+console.log(myArray[2]);
+
