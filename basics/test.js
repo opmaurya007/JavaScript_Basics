525a78677160d7703a89e3360225750017bc1bdb
OMPRAKASH KUSHWAHA <124498524+opmaurya007@users.noreply.github.com>
2023-09-01 15:21:01 +0000

    data-types

--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,7 @@
-console.log("opmaurya");
+// console.log("opmaurya");
+const accountId = 144553
+let accountEmail = "opmaurya"
+var accountPassword = "12345"
+accountCity = "Pune"
+// accountId = 2
+console.log(accountId);
