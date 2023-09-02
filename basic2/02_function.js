76ff3c3b5e5156d676ad222015d69645829152e4
OMPRAKASH KUSHWAHA <124498524+opmaurya007@users.noreply.github.com>
2023-09-02 20:31:30 +0000

    2nd fubction details

---
+++ basic2/02_function.js
@@ -0,0 +1,24 @@
+// function caculateCartPrice(...num1){
+//     return num1;
+// }
+function caculateCartPrice(val1,val2,...num1){
+    return num1;
+}
+// console.log(caculateCartPrice(200,400,800,1000));
+const user={
+    username:"op",price:299
+}
+function handleObject(anyObject) {
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);   
+}
+// handleObject(user);
+// handleObject({
+//     username:"sam",
+//     price:399
+// })
+const myNewArray=[200,300,500,40];
+function returnSecondValue(getArray){
+    return getArray;
+}
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([2,3,44,33,222,55]));
