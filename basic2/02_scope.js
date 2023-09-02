ec827f7928af541a86afd2edd73e1c28de4f827e
OMPRAKASH KUSHWAHA <124498524+opmaurya007@users.noreply.github.com>
2023-09-02 20:32:02 +0000

    var and let,conts scope

---
+++ basic2/02_scope.js
@@ -0,0 +1,19 @@
+function one(){
+    const username="opmaurya"
+    function two(){
+        const website="youTube";
+        // console.log(username);
+    }
+    // console.log(website);
+    two();
+}
+one();
+if(true){
+    const username = "opmaurya";
+    if(username==="opmaurya"){
+        const website=" youtube";
+        console.log(username + website);
+    }
+    // console.log(website);
+}
+// console.log(username);
