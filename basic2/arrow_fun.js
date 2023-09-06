83780f14e9c9354ebacb125c8a042a52052b31cb
OMPRAKASH KUSHWAHA <124498524+opmaurya007@users.noreply.github.com>
2023-09-06 18:28:15 +0000

    arrow function details

--- basic2/arrow_fun.js
+++ basic2/arrow_fun.js
@@ -2,8 +2,34 @@ const user={
     username:"op",price:399,
     welComeMessage: function(){
         console.log(`${this.username}, welcome to website`);
+        console.log(this);
     }
 }
-user.welComeMessage();
-user.username="sam";
-user.welComeMessage();
+// user.welComeMessage();
+// user.username="sam";
+// user.welComeMessage();
+console.log(this);
+// function chai(){
+//     let username="opmaurya"
+// console.log(this.username);
+// }
+// chai();
+//  const chai =function (){
+//     let username="opmaurya"
+// console.log(this.username);
+// }
+// chai();
+// const chai =()=>{
+//     let username="opmaurya"
+// console.log(this.username);
+// }
+// chai();
+// const addTwo = (num1,num2)=>{
+//     return num1 + num2;
+// }
+// console.log(addTwo(3,4));
+
+// const addTwo=(num1,num2)=>num1+num2
+// console.log(addTwo(3,4));
+const addTwo=(num1,num2)=>({username:"opmaurya"})
+console.log(addTwo());
