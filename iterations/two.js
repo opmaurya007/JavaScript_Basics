5ba61ce02f7f353a0209d0dab642cbd29f203ce6
OMPRAKASH KUSHWAHA <124498524+opmaurya007@users.noreply.github.com>
2023-09-07 18:12:16 +0000

    for in looop details

---
+++ iterations/two.js
@@ -0,0 +1,15 @@
+// for in lopp
+const myObject={
+    js:'javascripts',
+    cpp:'C++',
+    rb:'ruby',
+    swift:'swift by apple'
+}
+for (const key in myObject) {
+    // console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+//for in loop for array
+const programming=["js","rb",'py','java','cpp'];
+for(const key in programming){
+    // console.log(programming[key]);
+}
