413fb2160778dfa206baabbb569b793cdde428b1
OMPRAKASH KUSHWAHA <124498524+opmaurya007@users.noreply.github.com>
2023-09-07 06:38:20 +0000

    Immediately Invoked Function Expression(IIFE)

---
+++ basic2/iife.js
@@ -0,0 +1,10 @@
+//Immediately Invoked Function Expression(IIFE)
+
+//named iife
+// (function chai(){
+//     console.log("DB Connected");
+// })();
+((name)=>{
+    console.log(`DB Connected2 ${name}`);
+})("opmaurya")
+
