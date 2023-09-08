9a983499c4672f8fc7034f6b86254ab981c3a58c
OMPRAKASH KUSHWAHA <124498524+opmaurya007@users.noreply.github.com>
2023-09-08 12:43:32 +0000

    forEach details

---
+++ iterations/three.js
@@ -0,0 +1,36 @@
+// const coding=['js','ruby','java','python','cpp']
+// coding.forEach(  function(val){
+//     console.log(val);
+// })
+// coding.forEach((item)=>{
+//     console.log(item);
+// }) 
+// function printMe(item){
+//     console.log(item);
+// }
+// coding.forEach(printMe)
+ 
+// coding.forEach((item,index,arr)=>{
+//     console.log(item,index,arr);
+// })
+const myCoding=[{
+    laguageName:"javascripts",
+    languageFileName:"jsfiles"
+},
+{
+    laguageName:"java",
+    languageFileName:"jv"
+},{
+    laguageName:"html",
+    languageFileName:"htmlFile"
+}]
+// myCoding.forEach((item)=>{
+//     console.log(item.laguageName);
+// })
+
+// const coding=['js','ruby','java','python','cpp']
+// const values=coding.forEach((item)=>{
+//     // console.log(item);
+// return item;
+// })
+// console.log(values);
