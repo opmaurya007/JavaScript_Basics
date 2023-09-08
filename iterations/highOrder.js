91f803717c9ce9562e2ded09da62c434152c81fd
OMPRAKASH KUSHWAHA <124498524+opmaurya007@users.noreply.github.com>
2023-09-08 19:31:51 +0530

    Update highOrder.js

--- iterations/highOrder.js
+++ iterations/highOrder.js
@@ -1,12 +1,12 @@
 //for of 
-// const arr=[1,2,3,4,5];
-// for(const num of arr){
-//     console.log(num);
-// }
-// const greetings="Hello world";
-// for(const great of greetings){
-//     console.log(`Each is a ${great}`);
-// }
+const arr=[1,2,3,4,5];
+for(const num of arr){
+    console.log(num);
+}
+const greetings="Hello world";
+for(const great of greetings){
+    console.log(`Each is a ${great}`);
+}
 // map
 const map=new Map();
 map.set('IN',"India");
